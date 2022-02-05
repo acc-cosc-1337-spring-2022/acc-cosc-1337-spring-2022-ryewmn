c3c532c538e7345bf8a4b8f5f0a3e11eb4eb333a
ryewmn <ryanchristopher.rico@gmail.com>
2022-02-04 19:30:39 -0600

    Homework 01

--- src/homework/01_variables/variables.cpp
+++ src/homework/01_variables/variables.cpp
@@ -1,4 +1,5 @@
 #include "variables.h"
+int num_2 = 5;
 
 //example
 int add_numbers(int num1, int num2)
@@ -6,7 +7,17 @@ int add_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+// homework
 
 
+int multiply_number(int num01)
+
+
+{
+
+
+    return num01 * num_2;
+
+}
 
 
