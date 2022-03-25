796f3f7b61efabe86bf2fd58e1887088532105ce
ryewmn <ryanchristopher.rico@gmail.com>
2022-03-25 17:33:05 -0500

    Homework 6 completion

--- src/homework/tic_tac_toe/main.cpp
+++ src/homework/tic_tac_toe/main.cpp
@@ -1,6 +1,28 @@
+#include "tic_tac_toe.h"
 
-int main() 
+using std::string, std::cout, std::cin;
+int main()
 {
+    TicTacToe game;
+    string first_player;
+    char choice;
+    int position;
 
-	return 0;
+    do {
+        cout << "Enter first player (X or O)";
+        cin >> first_player;
+        game.start_game(first_player);
+
+        while (!game.game_over()) {
+            cout << "Enter position [1-9]";
+            cin >> position;
+            game.mark_board(position);
+            game.display_board();
+        }
+        cout << "Continue? Type Y: ";
+        cin >> choice;
+    } while (choice == 'Y'|| choice == 'y');
+
+
+    return 0;
 }
